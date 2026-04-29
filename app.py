5b8ba6d0e4df5a0e93156f43e3985e21ecb18d54
Abhijith Os <93033890+AkkuOS@users.noreply.github.com>
2026-04-29 12:50:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with open("model.pkl", "rb") as f:
 st.markdown("""
 <style>
 .stApp {
-    background-image: url("https://images.unsplash.com/photo-1505691938895-1758d7feb511?q=80&w=1600&auto=format&fit=crop&blur=50");
+    background-image: url("https://media.houseandgarden.co.uk/photos/66f2a3e9c5f02403e9deba9f/master/w_1024%2Cc_limit/Christian%2520Bense%2520Battersea13727.jpg");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
